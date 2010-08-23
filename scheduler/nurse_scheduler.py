ef207242950d953b2879034362c920d219031880
Matjaz Cepar <mcepar1@Caplja.(none)>
2010-08-23 12:56:13 +0200

    Issue #10: Implemented the loading of the previous month and also storing the old schedulers.

--- scheduler/nurse_scheduler.py
+++ scheduler/nurse_scheduler.py
@@ -7,11 +7,11 @@ import os
 
 
 class NurseScheduler:
-  FILES_DIR = os.path.join("scheduler","persistence")
+  FILES_DIR = os.path.join("scheduler","persistence", "nurses")
 
   def __init__(self, nurses, date, morning_shift, afternoon_shift, night_shift, double_shift):
     """
-    The default constuctor.
+    The default constructor.
       nurses: a list of all the nurses, that will be scheduled
       date: is the starting date of the scheduling
       moring_shift: is the number of nurses, that will be working the morning shift
@@ -24,6 +24,7 @@ class NurseScheduler:
     for nurse in nurses:
       self.nurses.append(person.Nurse(nurse))
       self.nurses[-1].add_month (date)
+    self.__get_previous_month(date)
     
     
     self.date = date
@@ -32,13 +33,15 @@ class NurseScheduler:
     self.night_shift = night_shift
     self.double_shift = double_shift
     
+    
+    
   def __get_previous_month(self, date):
     """
-    This reloads the privously scheduled month int the application.
+    This reloads the previously scheduled month into the application.
       date: is the current schedule date
     """
     
-    prev_date = date - datetime.timedelta(months = 1)
+    prev_date = datetime.date(day = 1, month = date.month, year = date.year) - datetime.timedelta (days = 1) 
     filename = str(prev_date.month) + '_' + str(prev_date.year) + '.dat'
     
     old_nurses = pickle.load(file(os.path.join(NurseScheduler.FILES_DIR, filename),'rb'))
