781ce419b51b5bd99bbd1b155c03843cb434cb8c
Matjaz Cepar <matjaz.cepar@gmail.com>
2011-08-13 08:09:10 +0200

    Issue #56: Fixed

--- utils/exporter.py
+++ utils/exporter.py
@@ -2,6 +2,11 @@
 
 import csv
 
+class Office2003Dialect (csv.excel):
+  def __init__(self):
+    csv.Dialect.__init__(self)
+    self.delimiter=';'
+
 def exportCSV(workplace_matrix, path):
   """
   Exports the grid into a CSV file.
@@ -9,7 +14,7 @@ def exportCSV(workplace_matrix, path):
     path: the whole path to the file, that will be saved
   """  
   
-  writer = csv.writer(file(path, 'wb'))
+  writer = csv.writer(file(path, 'wb'), dialect=Office2003Dialect())
     
   for scheduling_unit in sorted(workplace_matrix.keys()):
     writer.writerow((str(scheduling_unit), ''))
